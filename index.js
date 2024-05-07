c84fc278cdd02c54d6257e9b6b6519f9842eb1f9
truonggiang1934 <arranhay11nam1311@outlook.com>
2024-05-08 06:05:06 +0700

    commit

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-function maxArea(height) {
-  let maxArea = 0;
-  let left = 0;
-  let right = height.length - 1;
-  while (left < right) {
-    maxArea = Math.max(
-      maxArea,
-      Math.min(height[left], height[right]) * (right - left),
-    );
-    if (height[left] < height[right]) left++;
-    else right--;
-  }
-  return maxArea;
-}
+const currentDate = new Date();
+console.log(currentDate); // Fri 2024-04-05T09:17:00.000Z
