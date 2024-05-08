c92b0bf1131a9840fa6c94f684da93b1bc3a1456
truonggiang1934 <arranhay11nam1311@outlook.com>
2024-05-09 02:00:55 +0700

    hello

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const currentDate = new Date();
-console.log(currentDate); // Fri 2024-04-05T09:17:00.000Z
+function countSubstrings(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    extendPalindrome(i, i);
+    extendPalindrome(i, i + 1);
+  }
+  return count;
+  function extendPalindrome(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      count++;
+      left--;
+      right++;
+    }
+  }
+}
