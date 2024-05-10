a897884065d30a80d7fc8732e759af4d3a7d67e1
truonggiang1934 <arranhay11nam1311@outlook.com>
2024-05-10 12:55:41 +0700

    demo

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-function searchMatrix(matrix, target) {
-  if (matrix.length === 0 || matrix[0].length === 0) return false;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  let row = 0;
-  let col = cols - 1;
-  while (row < rows && col >= 0) {
-    if (matrix[row][col] === target) return true;
-    else if (matrix[row][col] < target) row++;
-    else col--;
-  }
-  return false;
-}
+// Fund Alice and sponsor accounts
+await aptos.fundAccount({
+  accountAddress: aliceAddress,
+  amount: ALICE_INITIAL_BALANCE,
+});
+await aptos.fundAccount({
+  accountAddress: sponsorAddress,
+  amount: SPONSOR_INITIAL_BALANCE,
+});
+
+// Show account balances
+const aliceBalanceBefore = await aptos.getAccountCoinsData({
+  accountAddress: aliceAddress,
+});
+const sponsorBalanceBefore = await aptos.getAccountCoinsData({
+  accountAddress: sponsorAddress,
+});
