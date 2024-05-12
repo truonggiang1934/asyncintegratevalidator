51b9453b5c9f9b7a0d1c1fc5128e93c1ac5e1d0c
truonggiang1934 <arranhay11nam1311@outlook.com>
2024-05-12 12:32:13 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const gnomeSort = (arr) => {
-  let index = 0;
-  while (index < arr.length) {
-    if (index === 0 || arr[index] >= arr[index - 1]) {
-      index++;
-    } else {
-      [arr[index], arr[index - 1]] = [arr[index - 1], arr[index]];
-      index--;
-    }
-  }
-  return arr;
-};
+const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
